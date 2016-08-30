5f444b1b115bde4b686e68320d5e3f4767fe3b52
Filip Dupanović <filip.dupanovic@gmail.com>
2016-08-30 19:03:10 +0000

    feat(src): Convert to React application

---
+++ src/components.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Main = () => (
+  <h1>Hello World!</h1>
+)
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import {Main} from './components';
+
+(() => {
+  const container = document.createElement('div')
+  document.body.appendChild(container)
+
+  ReactDOM.render(<Main />, container)
+})()
