58e6de5c669c4d603a518da8b85b8b1859ea3de2
Filip Dupanović <filip.dupanovic@gmail.com>
2016-09-01 14:00:49 +0200

    style(src): Remove extraneous

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import {Main} from './components';
+import {Main} from './components'
 
 (() => {
   const container = document.createElement('div')
