705e46dcc861a3427cbbc4b2a8ffc167f4cc5cdc
Filip Dupanović <filip.dupanovic@gmail.com>
2016-10-19 07:03:34 +0200

    fix(web): Use correct URLs

--- src/header/index.js
+++ src/header/index.js
@@ -14,8 +14,8 @@ export const Header = () => (
     <h1 className={styles.title}>Langri-Sha</h1>
     <nav className={styles.nav}>
       {[
-        ['Stack Overflow', 'https://stackoverflow.com/users/story/44041'],
-        ['Keybase', 'https://github.com/langri-sha'],
+        ['Stack Overflow', 'https://stackoverflow.com/users/44041/filip-dupanovi%C4%87?tab=profile'],
+        ['Keybase', 'https://keybase.io/langrisha'],
         ['GitHub', 'https://github.com/langri-sha']
       ].map(([name, href]) => (
         <Link key={name} name={name} href={href} />
