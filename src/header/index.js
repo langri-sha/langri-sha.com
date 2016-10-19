24e7ce353749ce26b89c8d8aaca8bc9fc5d29382
Filip Dupanović <filip.dupanovic@gmail.com>
2016-10-19 02:56:01 +0200

    fest(web): Stub links

--- src/header/index.js
+++ src/header/index.js
@@ -5,7 +5,7 @@ import {iconify} from '../lib/str'
 import styles from './styles'
 
 export const Link = ({name, href}) => (
-  <a href={href}>
+  <a className={styles.link} href={href}>
     <span className={styles[iconify(name)]} />
   </a>
 )
@@ -13,13 +13,14 @@ export const Link = ({name, href}) => (
 export const Header = () => (
   <header>
     <h1 className={styles.title}>Langri-Sha</h1>
-    {[
-      ['GitHub', 'https://github.com/langri-sha'],
-      ['Stack Overflow', 'https://github.com/langri-sha'],
-      ['Keybase', 'https://github.com/langri-sha'],
-      ['Twitter', 'https://github.com/langri-sha']
-    ].map(([name, href]) => (
-      <Link key={name} name={name} href={href} />
-    ))}
+    <nav className={styles.nav}>
+      {[
+        ['Stack Overflow', 'https://stackoverflow.com/users/story/44041'],
+        ['Keybase', 'https://github.com/langri-sha'],
+        ['GitHub', 'https://github.com/langri-sha'],
+      ].map(([name, href]) => (
+        <Link key={name} name={name} href={href} />
+      ))}
+    </nav>
   </header>
 )
