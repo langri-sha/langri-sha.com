e140304b760aa0c9af4f9196633f183eac80579c
Filip Dupanović <filip.dupanovic@gmail.com>
2016-10-19 15:36:07 +0200

    feat(header): Humanize links

--- src/header/index.js
+++ src/header/index.js
@@ -3,8 +3,8 @@ import React from 'react'
 import {iconify} from '../lib/str'
 import styles from './styles'
 
-export const Link = ({name, href}) => (
-  <a title={name} className={styles.link} onClick={() => window.open(href)}>
+export const Link = ({name, href, title}) => (
+  <a className={styles.link} href={href} title={title} target={'_blank'}>
     <span className={styles[iconify(name)]} />
   </a>
 )
@@ -14,11 +14,20 @@ export const Header = () => (
     <h1 className={styles.title}>Langri-Sha</h1>
     <nav className={styles.nav}>
       {[
-        ['Stack Overflow', 'https://stackoverflow.com/users/44041/filip-dupanovi%C4%87?tab=profile'],
-        ['Keybase', 'https://keybase.io/langrisha'],
-        ['GitHub', 'https://github.com/langri-sha']
-      ].map(([name, href]) => (
-        <Link key={name} name={name} href={href} />
+        [
+          'Stack Overflow', 'https://stackoverflow.com/users/44041/filip-dupanovi%C4%87?tab=profile',
+          'StackOverflow profile #SOreadytohelp 💓'
+        ],
+        [
+          'Keybase', 'https://keybase.io/langrisha',
+          'Identity details on Keybase.io'
+        ],
+        [
+          'GitHub', 'https://github.com/langri-sha',
+          'GitHub profile'
+        ]
+      ].map(([name, href, title]) => (
+        <Link key={name} name={name} href={href} title={title} />
       ))}
     </nav>
   </header>
