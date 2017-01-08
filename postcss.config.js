8e05d6d0f82f556dd88f587b714f8f2a6bd42352
Filip Dupanović <filip.dupanovic@gmail.com>
2017-01-08 04:44:06 +0100

    style: correct indentation

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,10 @@
 module.exports = (ctx) => ({
-    plugins: {
-      'postcss-smart-import': null,
-      'postcss-cssnext': null,
-      'postcss-font-smoothing': null,
-      'cssnano': null,
-      'postcss-reporter': null,
-      'postcss-browser-reporter': null
-    }
+  plugins: {
+    'postcss-smart-import': null,
+    'postcss-cssnext': null,
+    'postcss-font-smoothing': null,
+    'cssnano': null,
+    'postcss-reporter': null,
+    'postcss-browser-reporter': null
+  }
 })
