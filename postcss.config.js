98662eda60e3ccf676789d43fd8b61c2de3938ab
Filip Dupanović <filip.dupanovic@gmail.com>
2017-01-09 23:02:42 +0100

    chore: update postcss configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,29 @@
+const stylelint = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-css-modules'
+  ],
+  rules: {
+    'property-no-unknown': [ true, {
+      ignoreProperties: [
+        'composes',
+        'font-smoothing'
+      ]
+    }]
+  }
+}
+
 module.exports = (ctx) => ({
   plugins: {
-    'postcss-smart-import': null,
+    'postcss-import': {
+      plugins: [
+        require('stylelint')(stylelint)
+      ]
+    },
     'postcss-cssnext': null,
     'postcss-font-smoothing': null,
-    'cssnano': null,
-    'postcss-reporter': null,
-    'postcss-browser-reporter': null
+    'cssnano': prod ? null : false,
+    'postcss-reporter': dev ? null : false,
+    'postcss-browser-reporter': dev ? null : false
   }
 })
