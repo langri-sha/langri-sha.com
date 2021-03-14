dd805b97a227e80863d7fed73378993908bc9794
Filip Dupanović <filip.dupanovic@gmail.com>
2021-03-14 10:17:17 +0000

    feat(terraform): Add custom org admin role

--- terraform/org.tf
+++ terraform/org.tf
@@ -1,3 +1,11 @@
 data "google_organization" "org" {
   domain = var.org_domain
 }
+
+resource "google_organization_iam_custom_role" "org_admin" {
+  role_id     = "org-admin"
+  org_id      = data.google_organization.org.org_id
+  title       = "Organization Admins"
+  description = "Allows users to manage the organization and create root folders"
+  permissions = ["resourcemanager.folders.*", "resourcemanager.organizations.*"]
+}
