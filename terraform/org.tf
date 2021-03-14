ebffae51369172f192ab4f7cf51f589ab669998c
Filip Dupanović <filip.dupanovic@gmail.com>
2021-03-14 21:27:36 +0000

    feat(terraform): Bind billing admin roles
    Adds billing admins and assigns roles to create billing accounts.

--- terraform/org.tf
+++ terraform/org.tf
@@ -20,3 +20,11 @@ resource "google_organization_iam_member" "org_admin_folder_editor" {
   role    = "roles/resourcemanager.folderEditor"
   member  = each.value
 }
+
+resource "google_organization_iam_member" "org_billing_admin_billing_creator" {
+  for_each = toset(local.org_billing_admin_members)
+
+  org_id  = data.google_organization.org.org_id
+  role    = "roles/billing.creator"
+  member  = each.value
+}
