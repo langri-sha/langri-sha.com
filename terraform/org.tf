25f168530f685bdf9a19de900e56ab64db661fed
Filip Dupanović <filip.dupanovic@gmail.com>
2021-03-14 22:22:32 +0000

    feat(terraform): Use IAM bindings
    Use IAM bindings to restrict roles to certain members only.

--- terraform/org.tf
+++ terraform/org.tf
@@ -13,18 +13,14 @@ data "google_organization" "org" {
   domain = var.org_domain
 }
 
-resource "google_organization_iam_member" "org_admin_folder_editor" {
-  for_each = toset(local.org_admin_members)
-
+resource "google_organization_iam_binding" "org_admin_folder_editor" {
   org_id  = data.google_organization.org.org_id
   role    = "roles/resourcemanager.folderEditor"
-  member  = each.value
+  members = local.org_admin_members
 }
 
-resource "google_organization_iam_member" "org_billing_admin_billing_creator" {
-  for_each = toset(local.org_billing_admin_members)
-
+resource "google_organization_iam_binding" "org_billing_admin_billing_creator" {
   org_id  = data.google_organization.org.org_id
   role    = "roles/billing.creator"
-  member  = each.value
+  members = local.org_billing_admin_members
 }
