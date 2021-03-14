d40be3e9553962be4eadd263e13f6025fffe5846
Filip Dupanović <filip.dupanovic@gmail.com>
2021-03-14 12:14:01 +0000

    chore(terraform): Assort resources

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,9 @@
-variable "org_domain" {
-  type        = string
-  description = "Organization domain, for which resources are created."
-}
-
 variable "org_admin_members" {
   type        = string
   description = "Comma-separated list of users who are assigned the org-admin role."
+}
+
+variable "org_domain" {
+  type        = string
+  description = "Organization domain, for which resources are created."
 }
