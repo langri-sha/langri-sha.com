3d862ff2089a1b4e553a4860548a915f05d63ee1
Filip Dupanović <filip.dupanovic@gmail.com>
2021-03-20 22:34:29 +0000

    feat(terraform): Add billing module
    Adds module for providing providing the default billing account.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,8 @@
+variable "billing_account" {
+  type        = string
+  description = "Billing account ID, for the default billing account."
+}
+
 variable "org_admin_members" {
   type        = string
   description = "Comma-separated list of users who are assigned to administer the organization."
