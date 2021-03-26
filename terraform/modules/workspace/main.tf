7da208ae3063950759e85291148188f203fd3f78
Filip Dupanović <filip.dupanovic@gmail.com>
2021-03-26 06:23:12 +0000

    feat(terraform/workspace): Create TF admin project

--- terraform/modules/workspace/main.tf
+++ terraform/modules/workspace/main.tf
@@ -2,3 +2,17 @@ resource "google_folder" "workspace" {
   display_name = var.name
   parent       = "organizations/${var.org_id}"
 }
+
+module "project" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 10.2.2"
+
+  name = "tf-admin"
+
+  auto_create_network     = false
+  billing_account         = var.billing_account
+  default_service_account = "deprivilege"
+  folder_id               = google_folder.workspace.id
+  org_id                  = var.org_id
+  random_project_id       = "true"
+}
