10c339925fea2ef65fe24149582e658b7a6b304a
Filip Dupanović <filip.dupanovic@gmail.com>
2021-03-26 06:52:48 +0000

    feat(terraform): Upgrade google providers to latest
    Upgrades `hashicorp/google` and `hashicorp/google-beta` providers to
    their latest release.

--- terraform/org/versions.tf
+++ terraform/org/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "0.14.9"
 
   required_providers {
-    google      = "3.46.0"
-    google-beta = "3.46.0"
+    google      = "3.50.0"
+    google-beta = "3.50.0"
   }
 }
