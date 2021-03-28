0ecdb26ebb73d4fcf8b61ea62b9764aa80a6ec74
Filip Dupanović <filip.dupanovic@gmail.com>
2021-03-28 20:52:46 +0000

    feat(terraform/org): Output org_admins

--- terraform/modules/org/outputs.tf
+++ terraform/modules/org/outputs.tf
@@ -2,3 +2,8 @@ output "org_id" {
   description = "The Organization ID."
   value       = data.google_organization.org.org_id
 }
+
+output "org_admins" {
+  description = "List of users who are assigned to administer the organization."
+  value       = local.org_admins
+}
