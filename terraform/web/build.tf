995280e42139bb415957c4a1849180fa46358211
Filip Dupanović <filip.dupanovic@gmail.com>
2021-04-03 21:57:36 +0000

    feat(terraform/web): Create build project

---
+++ terraform/web/build.tf
@@ -0,0 +1,13 @@
+module "project_build" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 10.2.2"
+
+  name = "build"
+
+  auto_create_network     = false
+  billing_account         = data.terraform_remote_state.org.outputs.billing_account
+  default_service_account = "deprivilege"
+  folder_id               = data.terraform_remote_state.org.outputs.web_folder
+  org_id                  = data.terraform_remote_state.org.outputs.org_id
+  random_project_id       = "true"
+}
