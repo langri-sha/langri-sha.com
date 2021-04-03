8a5023f4912d5991953f61030380a8335a48c5af
Filip Dupanović <filip.dupanovic@gmail.com>
2021-04-03 21:32:51 +0000

    feat(terraform/web): Configure access to google providers
    Configures access to Google providers using the web workspace TF service
    account's access token.

---
+++ terraform/web/auth.tf
@@ -0,0 +1,22 @@
+module "access_token_resolver" {
+  source = "../modules/access-token-resolver"
+
+  target_service_account = data.terraform_remote_state.org.outputs.web_service_account_email
+
+  providers = {
+    google = google.access_token_resolver
+  }
+}
+
+
+provider "google" {
+  alias  = "access_token_resolver"
+}
+
+provider "google" {
+  access_token = module.access_token_resolver.access_token
+}
+
+provider "google-beta" {
+  access_token = module.access_token_resolver.access_token
+}
