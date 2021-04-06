31d501f46e52bcf5074b82625ea8563b3401e1b3
Filip Dupanović <filip.dupanovic@gmail.com>
2021-04-07 00:35:05 +0200

    feat(terraform/web): Add repo variables

---
+++ terraform/web/variables.tf
@@ -0,0 +1,11 @@
+variable "repo_name" {
+  default     = "langri-sha.com"
+  description = "Name of the GitHub monorepo."
+  type        = string
+}
+
+variable "repo_owner" {
+  default     = "langri-sha"
+  description = "Owner of the GitHub monorepo."
+  type        = string
+}
