3a4000d1013d401abf16a74ab2b0f27e0e834770
Filip Dupanović <filip.dupanovic@gmail.com>
2021-04-10 12:09:26 +0200

    feat(terraform/web): Hoist projects to main module
    Hoist project resources to main module.

--- terraform/web/build.tf
+++ terraform/web/build.tf
@@ -1,23 +1,3 @@
-module "project_build" {
-  source  = "terraform-google-modules/project-factory/google"
-  version = "~> 10.2.2"
-
-  name = "build"
-
-  auto_create_network     = false
-  billing_account         = data.terraform_remote_state.org.outputs.billing_account
-  default_service_account = "deprivilege"
-  folder_id               = data.terraform_remote_state.org.outputs.web_folder
-  org_id                  = data.terraform_remote_state.org.outputs.org_id
-  random_project_id       = "true"
-
-  activate_apis = [
-    "cloudbuild.googleapis.com",
-    "compute.googleapis.com",
-    "containerregistry.googleapis.com",
-  ]
-}
-
 module "cloudbuild" {
   source = "../modules/cloudbuild"
 
--- terraform/web/main.tf
+++ terraform/web/main.tf
@@ -42,3 +42,23 @@ data "terraform_remote_state" "org" {
     }
   }
 }
+
+module "project_build" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 10.2.2"
+
+  name = "build"
+
+  auto_create_network     = false
+  billing_account         = data.terraform_remote_state.org.outputs.billing_account
+  default_service_account = "deprivilege"
+  folder_id               = data.terraform_remote_state.org.outputs.web_folder
+  org_id                  = data.terraform_remote_state.org.outputs.org_id
+  random_project_id       = "true"
+
+  activate_apis = [
+    "cloudbuild.googleapis.com",
+    "compute.googleapis.com",
+    "containerregistry.googleapis.com",
+  ]
+}
