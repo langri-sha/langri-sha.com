9a13b96acecf21af68a290305a7039c151cf3551
Filip Dupanović <filip.dupanovic@gmail.com>
2021-04-11 21:31:27 +0200

    chore(terraform/org): Assort outputs

--- terraform/modules/org/outputs.tf
+++ terraform/modules/org/outputs.tf
@@ -1,8 +1,3 @@
-output "org_id" {
-  description = "The Organization ID."
-  value       = data.google_organization.org.org_id
-}
-
 output "org_admins" {
   description = "List of users who are assigned to administer the organization."
   value       = local.org_admins
@@ -11,4 +6,9 @@ output "org_admins" {
 output "org_billing_admins" {
   description = "List of users who are assigned to administer the billing accounts in the organization."
   value       = local.org_billing_admins
+}
+
+output "org_id" {
+  description = "The Organization ID."
+  value       = data.google_organization.org.org_id
 }
