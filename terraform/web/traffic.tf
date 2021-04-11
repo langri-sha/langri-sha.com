10cb8ff3c643c2f4b47c5e9361e0b2ec38e5ec64
Filip Dupanović <filip.dupanovic@gmail.com>
2021-04-12 00:20:05 +0200

    feat(terraform/web): Add domain records

--- terraform/web/traffic.tf
+++ terraform/web/traffic.tf
@@ -8,3 +8,15 @@ resource "google_dns_managed_zone" "default" {
   dns_name = "${data.terraform_remote_state.org.outputs.org_domain}."
   project  = module.project_edge.project_id
 }
+
+resource "google_dns_record_set" "default" {
+  for_each = local.host_subdomains
+
+  name    = "${each.value}${google_dns_managed_zone.default.dns_name}"
+  project = module.project_edge.project_id
+
+  managed_zone = google_dns_managed_zone.default.name
+  rrdatas      = [google_compute_global_address.default.address]
+  ttl          = 300
+  type         = "A"
+}
