c5821eac90b30a8fb283771efaa7c03b788b8ab4
Filip Dupanović <filip.dupanovic@gmail.com>
2021-04-11 23:05:01 +0200

    feat(terraform/web): Add DNS managed zone

--- terraform/web/traffic.tf
+++ terraform/web/traffic.tf
@@ -2,3 +2,9 @@ resource "google_compute_global_address" "default" {
   name    = "global-address"
   project = module.project_edge.project_id
 }
+
+resource "google_dns_managed_zone" "default" {
+  name     = "langri-sha"
+  dns_name = "${data.terraform_remote_state.org.outputs.org_domain}."
+  project  = module.project_edge.project_id
+}
