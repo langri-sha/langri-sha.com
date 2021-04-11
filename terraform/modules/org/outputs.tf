a4bd83456826a04edee10331a246cc0e827ec192
Filip Dupanović <filip.dupanovic@gmail.com>
2021-04-11 21:44:00 +0200

    chore(terraform/org): Rename output variables

--- terraform/modules/org/outputs.tf
+++ terraform/modules/org/outputs.tf
@@ -1,11 +1,11 @@
-output "org_admins" {
+output "admins" {
   description = "List of users who are assigned to administer the organization."
-  value       = local.org_admins
+  value       = local.admins
 }
 
-output "org_billing_admins" {
+output "billing_admins" {
   description = "List of users who are assigned to administer the billing accounts in the organization."
-  value       = local.org_billing_admins
+  value       = local.billing_admins
 }
 
 output "org_id" {
