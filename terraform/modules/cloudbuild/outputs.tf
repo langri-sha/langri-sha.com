5badb939208e673a5dfc9811312e9e79527278db
Filip Dupanović <filip.dupanovic@gmail.com>
2021-04-12 23:53:42 +0200

    fix(terraform/cloudbuild): Output SA email
    Outputs email, instead of entire SA object.

--- terraform/modules/cloudbuild/outputs.tf
+++ terraform/modules/cloudbuild/outputs.tf
@@ -1,4 +1,4 @@
-output "service_account" {
-  value = google_project_service_identity.cloudbuild
-  description = "Cloud Build service account for project."
+output "service_account_email" {
+  value = google_project_service_identity.cloudbuild.email
+  description = "Cloud Build service account email for the project."
 }
