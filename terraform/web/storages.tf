05dcc6682d15a39b6ea42bfbbf5d603d4e8c6cff
Filip Dupanović <filip.dupanovic@gmail.com>
2021-04-13 21:31:39 +0200

    feat(terraform/web): Configure public buckets IAM

--- terraform/web/storages.tf
+++ terraform/web/storages.tf
@@ -1,3 +1,19 @@
+data "google_iam_policy" "public_storage_bucket" {
+  binding {
+    members = [
+      "allUsers",
+    ]
+    role = "roles/storage.objectViewer"
+  }
+
+  binding {
+    members = [
+      "serviceAccount:${module.cloudbuild.service_account_email}"
+    ]
+    role = "roles/storage.objectAdmin"
+  }
+}
+
 resource "google_storage_bucket" "public" {
   for_each = toset(compact([
     for host in local.hosts : contains(keys(local.host_redirects), host) ? "" : host
@@ -6,7 +22,8 @@ resource "google_storage_bucket" "public" {
   name    = local.host_names[each.value]
   project = module.project_edge.project_id
 
-  requester_pays = true
+  requester_pays              = true
+  uniform_bucket_level_access = true
 
   cors {
     max_age_seconds = 60
@@ -18,4 +35,13 @@ resource "google_storage_bucket" "public" {
     main_page_suffix = "index.html"
     not_found_page   = "404.html"
   }
+}
+
+resource "google_storage_bucket_iam_policy" "default" {
+  for_each = toset(compact([
+    for host in local.hosts : contains(keys(local.host_redirects), host) ? "" : host
+  ]))
+
+  bucket      = google_storage_bucket.public[each.value].name
+  policy_data = data.google_iam_policy.public_storage_bucket.policy_data
 }
