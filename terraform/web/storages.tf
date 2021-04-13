7cf643889447d27326ae10f6d3f364f7a67d38e7
Filip Dupanović <filip.dupanovic@gmail.com>
2021-04-13 20:49:40 +0200

    feat(terraform/web): Create host storages

---
+++ terraform/web/storages.tf
@@ -0,0 +1,15 @@
+resource "google_storage_bucket" "public" {
+  for_each = toset(compact([
+    for host in local.hosts : contains(keys(local.host_redirects), host) ? "" : host
+  ]))
+
+  name    = local.host_names[each.value]
+  project = module.project_edge.project_id
+
+  requester_pays = true
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
