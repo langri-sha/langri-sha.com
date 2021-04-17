947a1bbdc40e8b568776aad362d7e556b382c2a2
Filip Dupanović <filip.dupanovic@gmail.com>
2021-04-17 10:21:18 +0000

    feat(terraform/web): Assort resources
    Assorts resources, baset on significance.

--- terraform/web/main.tf
+++ terraform/web/main.tf
@@ -46,40 +46,6 @@ locals {
   }
 }
 
-provider "google" {
-  alias = "access_token_resolver"
-}
-
-module "access_token_resolver" {
-  source = "../modules/access-token-resolver"
-
-  target_service_account = local.web_service_account_email
-
-  providers = {
-    google = google.access_token_resolver
-  }
-}
-
-provider "google" {
-  access_token = module.access_token_resolver.access_token
-}
-
-provider "google-beta" {
-  access_token = module.access_token_resolver.access_token
-}
-
-data "terraform_remote_state" "org" {
-  backend = "remote"
-
-  config = {
-    hostname     = "app.terraform.io"
-    organization = "langri-sha"
-    workspaces = {
-      name = "org"
-    }
-  }
-}
-
 module "project_build" {
   source  = "terraform-google-modules/project-factory/google"
   version = "~> 10.2.2"
@@ -116,4 +82,38 @@ module "project_edge" {
     "compute.googleapis.com",
     "dns.googleapis.com",
   ]
+}
+
+provider "google" {
+  alias = "access_token_resolver"
+}
+
+provider "google" {
+  access_token = module.access_token_resolver.access_token
+}
+
+provider "google-beta" {
+  access_token = module.access_token_resolver.access_token
+}
+
+module "access_token_resolver" {
+  source = "../modules/access-token-resolver"
+
+  target_service_account = local.web_service_account_email
+
+  providers = {
+    google = google.access_token_resolver
+  }
+}
+
+data "terraform_remote_state" "org" {
+  backend = "remote"
+
+  config = {
+    hostname     = "app.terraform.io"
+    organization = "langri-sha"
+    workspaces = {
+      name = "org"
+    }
+  }
 }
