b92aec782d28987a09ad7e1ff1ba36c03966553e
Filip Dupanović <filip.dupanovic@gmail.com>
2021-04-25 22:03:05 +0000

    feat(terraform/build): Add WS PR build trigger

--- terraform/web/build.tf
+++ terraform/web/build.tf
@@ -5,15 +5,29 @@ module "cloudbuild" {
   repo_name  = var.repo_name
   repo_owner = var.repo_owner
 
-  triggers = [{
-    description = "Terraform Pull Request"
-    filename    = "terraform/cloudbuild.yaml"
-    name        = "terraform-pull-request"
+  triggers = [
+    {
+      description = "Terraform Pull Request"
+      filename    = "terraform/cloudbuild.yaml"
+      name        = "terraform-pull-request"
 
-    included_files = ["docker-compose.yml", "terraform/**"]
+      included_files = ["docker-compose.yml", "terraform/**"]
 
-    pull_request = {
-      branch = ".*"
+      pull_request = {
+        branch = ".*"
+      }
+    },
+
+    {
+      description = "Workspace Pull Request"
+      filename    = "cloudbuild.yaml"
+      name        = "workspace-pull-request"
+
+      ignored_files = ["terraform/**"]
+
+      pull_request = {
+        branch = ".*"
+      }
     }
-  }]
+  ]
 }
