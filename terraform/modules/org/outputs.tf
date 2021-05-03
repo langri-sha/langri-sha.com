2b9729f6f906a62cece19e0437e787ed9eb4a90b
Filip Dupanović <filip.dupanovic@gmail.com>
2021-05-03 20:37:03 +0000

    feat(terraform/org): Add org TF SA

--- terraform/modules/org/outputs.tf
+++ terraform/modules/org/outputs.tf
@@ -27,3 +27,8 @@ output "project_id" {
   description = "Main organization project ID."
   value       = module.project_org.project_id
 }
+
+output "service_account_email" {
+  description = "Email of the Terrraform service account managing the organization."
+  value       = google_service_account.terraform.email
+}
