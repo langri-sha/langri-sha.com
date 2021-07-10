63c1765e2a12c626f776cdf2beca5e59fd21e08c
Filip Dupanović <filip.dupanovic@gmail.com>
2021-07-10 21:20:12 +0000

    feat(org): Add `dns_managed_zone` to outputs

--- terraform/modules/public-dns/outputs.tf
+++ terraform/modules/public-dns/outputs.tf
@@ -1,6 +1,6 @@
 output "dns_managed_zone" {
   description = "Public DNS managed zone ID"
-  value = google_dns_managed_zone.public.id
+  value       = google_dns_managed_zone.public.name
 }
 
 output "name_servers" {
