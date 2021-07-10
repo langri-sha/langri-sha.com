866eb0269cd7a74887f3e65c19fd6ec6eff9226a
Filip Dupanović <filip.dupanovic@gmail.com>
2021-07-10 21:23:06 +0000

    feat(web): Remove site verifications
    Migrates creating DNS records to the next public DNS managed zone.

--- terraform/org/outputs.tf
+++ terraform/org/outputs.tf
@@ -38,11 +38,6 @@ output "org_project_id" {
   value       = module.org.project_id
 }
 
-output "site_verifications" {
-  description = "List of DNS-based domain ownership verifications."
-  value       = compact(split(",", var.site_verifications))
-}
-
 output "web_folder" {
   description = "Folder where workspace resources are created."
   value       = module.web.folder
