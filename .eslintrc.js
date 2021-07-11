fb58f5a3af4879750897cdb6ba6b39e6ac5bdc7b
Filip Dupanović <filip.dupanovic@gmail.com>
2021-07-11 20:46:02 +0000

    feat(eslint): Configure ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+// @flow
+const path = require('path')
+
+module.exports = {
+  settings: {
+    resolvePluginsRelativeTo: path.dirname(
+      require.resolve('@langri-sha/eslint-config')
+    ),
+  },
+  extends: ['@langri-sha/eslint-config'],
+}
