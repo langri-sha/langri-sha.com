86fcaa1aabc306f3eb2db29ad809d4b917e27910
Filip Dupanović <filip.dupanovic@gmail.com>
2021-07-11 15:46:31 +0000

    feat(lint-staged): Add Prettier config
    Updates `lint-staged` with Prettier usage.

---
+++ lint-staged.config.js
@@ -0,0 +1,23 @@
+// @flow
+const prettier = require('prettier')
+
+/* ::
+type Config = {
+  [pattern: string]:
+    | string
+    | string[]
+    | ((files: string[]) => string | string[] | Promise<string | string[]>)
+}
+*/
+
+module.exports = ({
+  '*.{css,html,json,md,yaml,yml}': async (files) => {
+    const options = { ignorePath: './.prettierignore' }
+    const promises = files.map((file) => prettier.getFileInfo(file, options))
+    const results = await Promise.all(promises)
+    const filteredFiles = files.filter((_, index) => !results[index].ignored)
+    return filteredFiles.length
+      ? `prettier --write ${filteredFiles.join(' ')}`
+      : []
+  },
+} /*: Config */)
