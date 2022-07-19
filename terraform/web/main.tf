32c90634dbe6a14813686f63f740bc8704bce961
Filip Dupanović <filip.dupanovic@gmail.com>
2022-07-19 23:53:03 +0000

    feat(terraform/web): Hoist triggers to main module
    Hoists configuring Cloud Build triggers to the main module.

--- terraform/web/main.tf
+++ terraform/web/main.tf
@@ -53,6 +53,89 @@ locals {
     "preview"           = [local.host_urls["preview"]],
     "preview-assets"    = [local.host_urls["preview"]],
   }
+
+  triggers = [
+    {
+      description = "Terraform Pull Request"
+      filename    = "terraform/cloudbuild.yaml"
+      name        = "terraform-pull-request"
+
+      included_files = ["docker-compose.yml", "terraform/**"]
+
+      pull_request = {
+        branch = ".*"
+      }
+    },
+
+    {
+      description = "Workspace Pull Request"
+      filename    = "cloudbuild.yaml"
+      name        = "workspace-pull-request"
+
+      substitutions = {
+        "_PREVIEW_ASSETS_BUCKET"    = google_storage_bucket.public["preview-assets"].name
+        "_PREVIEW_ASSETS_URL"       = local.host_urls["preview-assets"]
+        "_PREVIEW_BUCKET"           = google_storage_bucket.public["preview"].name
+        "_PREVIEW_URL"              = local.host_urls["preview"]
+        "_PRODUCTION_ASSETS_BUCKET" = google_storage_bucket.public["production-assets"].name
+        "_PRODUCTION_ASSETS_URL"    = local.host_urls["production-assets"]
+        "_PRODUCTION_BUCKET"        = google_storage_bucket.public["production"].name
+        "_PRODUCTION_URL"           = local.host_urls["production"]
+      }
+
+      ignored_files = ["terraform/**"]
+
+      pull_request = {
+        branch = ".*"
+      }
+    },
+
+    {
+      description = "Workspace Push"
+      filename    = "cloudbuild.yaml"
+      name        = "workspace-push"
+
+      substitutions = {
+        "_PREVIEW_ASSETS_BUCKET"    = google_storage_bucket.public["preview-assets"].name
+        "_PREVIEW_ASSETS_URL"       = local.host_urls["preview-assets"]
+        "_PREVIEW_BUCKET"           = google_storage_bucket.public["preview"].name
+        "_PREVIEW_URL"              = local.host_urls["preview"]
+        "_PRODUCTION_ASSETS_BUCKET" = google_storage_bucket.public["production-assets"].name
+        "_PRODUCTION_ASSETS_URL"    = local.host_urls["production-assets"]
+        "_PRODUCTION_BUCKET"        = google_storage_bucket.public["production"].name
+        "_PRODUCTION_URL"           = local.host_urls["production"]
+      }
+
+      ignored_files = ["terraform/**"]
+
+      push = {
+        branch = "main"
+      }
+    },
+
+    {
+      description = "Workspace Release"
+      filename    = "cloudbuild.yaml"
+      name        = "workspace-release"
+
+      substitutions = {
+        "_PREVIEW_ASSETS_BUCKET"    = google_storage_bucket.public["preview-assets"].name
+        "_PREVIEW_ASSETS_URL"       = local.host_urls["preview-assets"]
+        "_PREVIEW_BUCKET"           = google_storage_bucket.public["preview"].name
+        "_PREVIEW_URL"              = local.host_urls["preview"]
+        "_PRODUCTION_ASSETS_BUCKET" = google_storage_bucket.public["production-assets"].name
+        "_PRODUCTION_ASSETS_URL"    = local.host_urls["production-assets"]
+        "_PRODUCTION_BUCKET"        = google_storage_bucket.public["production"].name
+        "_PRODUCTION_URL"           = local.host_urls["production"]
+      }
+
+      ignored_files = ["terraform/**"]
+
+      push = {
+        tag = ".*"
+      }
+    }
+  ]
 }
 
 module "project_build" {
