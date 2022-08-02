6e8a8da31036e5aa3d56a69a146159ec57d1d9c8
Filip Dupanović <filip.dupanovic@gmail.com>
2022-08-02 19:37:57 +0000

    feat(terraform/org): Upgrade Google provider to latest

--- terraform/modules/org/main.tf
+++ terraform/modules/org/main.tf
@@ -9,7 +9,7 @@ data "google_organization" "org" {
 
 module "project_org" {
   source  = "terraform-google-modules/project-factory/google"
-  version = "~> 10.2.2"
+  version = "~> 13.0.0"
 
   name = "organization"
 
