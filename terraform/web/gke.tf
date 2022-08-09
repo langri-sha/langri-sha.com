f14650c3267531527dd28a224b194428478c042d
Filip Dupanović <filip.dupanovic@gmail.com>
2022-08-09 10:56:17 +0000

    feat(terraform/web): Add bastion host
    Adds bastion host for thje web network.

---
+++ terraform/web/gke.tf
@@ -0,0 +1,14 @@
+module "iap_bastion" {
+  source  = "terraform-google-modules/bastion-host/google"
+  version = "~> 5.0.0"
+
+  project = module.project_edge.project_id
+  zone    = local.zone
+  network = google_compute_network.vpc.self_link
+  subnet  = google_compute_subnetwork.subnet.self_link
+
+  members = concat(
+    local.admin_members,
+    ["serviceAccount:${local.web_service_account_email}"]
+  )
+}
