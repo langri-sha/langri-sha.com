dd15b39c9b01dc4395da975fbfe5614496172f75
Filip Dupanović <filip.dupanovic@gmail.com>
2022-09-18 23:50:59 +0200

    feat(terraform): Drop repo modules
    Sources modules from
    github.com/langri-sha/terraform-google-cloud-platform and drops the repo
    modules.

--- terraform/modules/workspace/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "folder" {
-  description = "Folder where workspace resources are created."
-  value       = google_folder.workspace.name
-}
-
-output "service_account_email" {
-  description = "Email of the Terrraform service account managing the workspace."
-  value       = google_service_account.terraform.email
-}
