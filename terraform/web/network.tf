d33a9441dd80d0855afbaf96dd09c13d7f8aab9c
Filip Dupanović <filip.dupanovic@gmail.com>
2023-01-08 11:55:44 +0000

    feat(terraform): Add support for creating VPC networks

---
+++ terraform/web/network.tf
@@ -0,0 +1,11 @@
+module "vpc" {
+  for_each = local.vpc
+  source   = "terraform-google-modules/network/google"
+  version  = "~> 6.0"
+
+  network_name     = each.key
+  project_id       = each.value.project
+  routing_mode     = each.value.routing_mode
+  secondary_ranges = each.value.secondary_ranges
+  subnets          = each.value.subnets
+}
