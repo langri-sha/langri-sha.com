412557032888af0ea06ae66f69497ffd9b6fe15f
Filip Dupanović <filip.dupanovic@gmail.com>
2024-04-09 14:19:03 +0200

    feat(webpack): Add reusable resolve loader

--- packages/webpack/src/index.js
+++ packages/webpack/src/index.js
@@ -1,5 +1,8 @@
 // @flow
 
+import path from 'path'
+import type { ResolveOptions } from 'webpack'
+
 // Stock.
 export { default } from 'webpack'
 export const { EnvironmentPlugin } = require('webpack')
@@ -10,3 +13,8 @@ export { CleanWebpackPlugin as CleanPlugin } from 'clean-webpack-plugin'
 export { default as CopyPlugin } from 'copy-webpack-plugin'
 export { default as HtmlPlugin } from 'html-webpack-plugin'
 export { default as TerserPlugin } from 'terser-webpack-plugin'
+
+// Resolve Webpack loaders from this package first.
+export const resolveLoader: ResolveOptions = {
+  modules: [path.join(__dirname, '..', 'node_modules'), 'node_modules'],
+}
