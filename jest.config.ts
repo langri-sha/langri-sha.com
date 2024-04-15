50731fbb6f33ce81425deb64f9adf1213bef11ab
Filip Dupanović <filip.dupanovic@gmail.com>
2024-04-15 14:50:05 +0200

    feat(jest): Configure ignore patterns

--- jest.config.ts
+++ jest.config.ts
@@ -1,1 +1,19 @@
-export { default } from '@langri-sha/jest-config'
+import type { Config } from 'jest'
+import defaults from '@langri-sha/jest-config'
+
+const config: Config = {
+  ...defaults,
+  transformIgnorePatterns: [
+    `node_modules/(?!(?:.pnpm/)?(${[
+      'find-up',
+      'locate-path',
+      'p-limit',
+      'p-locate',
+      'path-exists',
+      'unicorn-magic',
+      'yocto-queue',
+    ].join('|')}))`,
+  ],
+}
+
+export default config
