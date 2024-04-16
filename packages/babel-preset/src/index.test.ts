5c250c87542a9785f500b25410e5fa5e5214eb65
Filip Dupanović <filip.dupanovic@gmail.com>
2024-04-16 21:32:14 +0200

    feat(babel-preset): Add tests

---
+++ packages/babel-preset/src/index.test.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@langri-sha/jest-test'
+
+import { loadPresetPlugins } from '@langri-sha/babel-test'
+
+test('default preset plugins', async () => {
+  for (const environment of ['development', 'production']) {
+    const plugins = await loadPresetPlugins(
+      environment,
+      '@langri-sha/babel-preset'
+    )
+
+    expect(plugins).toMatchSnapshot(`${environment} environment`)
+  }
+})
