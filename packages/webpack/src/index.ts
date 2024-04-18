caeb5a7fab212a84ff3c3298a26855064aaed4ac
Filip Dupanović <filip.dupanovic@gmail.com>
2024-04-18 14:04:38 +0200

    feat(webpack): Add reusable resolve settings

--- packages/webpack/src/index.ts
+++ packages/webpack/src/index.ts
@@ -12,6 +12,13 @@ export { default as CopyPlugin } from 'copy-webpack-plugin'
 export { default as HtmlPlugin } from 'html-webpack-plugin'
 export { default as TerserPlugin } from 'terser-webpack-plugin'
 
+/**
+ * Reusable resolve settings.
+ */
+export const resolve = {
+  extensions: ['.tsx', '.ts', '.js'],
+}
+
 // Resolve Webpack loaders from this package first.
 export const resolveLoader: ResolveOptions = {
   modules: [path.join(__dirname, '..', 'node_modules'), 'node_modules'],
