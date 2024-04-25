af296ddf80b992191412f4a3d3bc5fc1d9534bde
Filip Dupanović <filip.dupanovic@gmail.com>
2024-04-25 19:53:42 +0200

    feat(projen-project): Configure Git ignore options

--- packages/projen/src/index.ts
+++ packages/projen/src/index.ts
@@ -7,6 +7,46 @@ export interface ProjectOptions extends BaseProjectOptions {}
 
 export class Project extends BaseProject {
   constructor(options: ProjectOptions) {
-    super(options)
+    super({
+      ...options,
+      gitIgnoreOptions: getGitIgnoreOptions(options),
+    })
   }
 }
+
+const getGitIgnoreOptions = (
+  options: ProjectOptions,
+): ProjectOptions['gitIgnoreOptions'] => ({
+  ...options.gitIgnoreOptions,
+  ignorePatterns: [
+    ...`
+    .*
+    !.babelrc
+    !.dockerignore
+    !.editorconfig
+    !.eslintignore
+    !.eslintrc.js
+    !.gitattributes
+    !.gitignore
+    !.gitkeep
+    !.husky
+    !.npmignore
+    !.openssl
+    !.prettierignore
+    !.terraform.lock.hcl
+    *.log
+    *.tsbuildinfo
+
+    !.github/
+    !.husky/
+    !.projen/
+    dist/
+    local/
+    node_modules/
+    `
+      .split('\n')
+      .map((pattern) => pattern.trim())
+      .filter((pattern) => pattern.length > 0),
+    ...(options.gitIgnoreOptions?.ignorePatterns ?? []),
+  ],
+})
