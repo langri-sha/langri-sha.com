eb24ed336a9cdde75ccd0b6802cca9303ae10e6c
Filip Dupanović <filip.dupanovic@gmail.com>
2024-05-05 21:31:09 +0200

    feat(codeowners): Manage `CODEOWNERS` files
    Adds support for managing `CODEOWNERS` files with projen.

--- packages/projen-codeowners/src/index.ts
+++ packages/projen-codeowners/src/index.ts
@@ -1,1 +1,28 @@
-export {}
+import { Component, type Project, TextFile } from 'projen'
+
+/**
+ * Codeowners options.
+ */
+export interface CodeownersOptions {
+  [pattern: string]: string | string[]
+}
+
+/**
+ * A component for maintaining the CODEOWNERS file.
+ */
+export class Codeowners extends Component {
+  constructor(project: Project, options?: CodeownersOptions) {
+    super(project)
+
+    new TextFile(project, 'CODEOWNERS', {
+      marker: true,
+      readonly: true,
+      lines: Object.entries(options ?? {})
+        .map(
+          ([pattern, owners]) =>
+            [pattern, Array.isArray(owners) ? owners : [owners]] as const,
+        )
+        .map(([pattern, owners]) => `${pattern} ${owners.join(' ')}`),
+    })
+  }
+}
