c671f30c29273ae7bd4c81a0ddcaae509cab37f4
Filip Dupanović <filip.dupanovic@gmail.com>
2024-05-05 17:52:06 +0200

    feat(renovate): Create Renovate configuration

---
+++ packages/projen-renovate/src/index.test.ts
@@ -0,0 +1,16 @@
+import { synthSnapshot } from 'projen/lib/util/synth'
+import { expect, test } from '@langri-sha/jest-test'
+
+import { Project } from 'projen'
+import { Renovate } from './index'
+
+test('defaults', () => {
+  const project = new Project({
+    name: 'test-project',
+  })
+
+  new Renovate(project, {})
+
+  project.synth()
+  expect(synthSnapshot(project)).toMatchSnapshot()
+})
--- packages/projen-renovate/src/index.ts
+++ packages/projen-renovate/src/index.ts
@@ -1,1 +1,24 @@
-export {}
+import { type RenovateConfig } from 'renovate/dist/config/types'
+
+import { Component, JsonFile, type Project } from 'projen'
+
+/**
+ * Renovate configuration options.
+ */
+export interface RenovateOptions extends RenovateConfig {}
+
+/**
+ * A component for managing Renovate configurations.
+ */
+export class Renovate extends Component {
+  constructor(project: Project, options?: RenovateOptions) {
+    super(project)
+
+    new JsonFile(project, 'renovate.json', {
+      obj: {
+        $schema: 'https://docs.renovatebot.com/renovate-schema.json',
+        ...options,
+      },
+    })
+  }
+}
