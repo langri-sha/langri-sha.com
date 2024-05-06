e411f82e87cb1ab7f2bb9affb3e88185eabf5402
Filip Dupanović <filip.dupanovic@gmail.com>
2024-05-06 02:29:55 +0200

    feat(projen-codeowners): Add projen marker
    Adds projen marker to code owners file.

--- packages/projen-codeowners/src/index.ts
+++ packages/projen-codeowners/src/index.ts
@@ -14,15 +14,29 @@ export class Codeowners extends Component {
   constructor(project: Project, options?: CodeownersOptions) {
     super(project)
 
-    new TextFile(project, 'CODEOWNERS', {
+    const file = new TextFile(project, 'CODEOWNERS', {
       marker: true,
       readonly: true,
-      lines: Object.entries(options ?? {})
+    })
+
+    if (file.marker) {
+      file.addLine(`# ${file.marker}`)
+    }
+
+    for (const line of this.#getLines(options)) {
+      file.addLine(line)
+    }
+  }
+
+  #getLines(options?: CodeownersOptions) {
+    return [
+      ...Object.entries(options ?? {})
         .map(
           ([pattern, owners]) =>
             [pattern, Array.isArray(owners) ? owners : [owners]] as const,
         )
         .map(([pattern, owners]) => `${pattern} ${owners.join(' ')}`),
-    })
+      '',
+    ]
   }
 }
