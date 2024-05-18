701006a534aeffbf3525721fbea4b1f4c5736a30
Filip Dupanović <filip.dupanovic@gmail.com>
2024-05-18 10:47:52 +0300

    fix(project): Add looser dependency matching

--- packages/projen-project/src/index.ts
+++ packages/projen-project/src/index.ts
@@ -289,6 +289,7 @@ export class Project extends BaseProject {
           fileMatch: ['\\.?projen.*.(js|cjs|mjs|ts|mts|cts)$'],
           matchStrings: [
             "\\.(?<depType>addDep|addDevDep|addPeerDep)('(?<depName>[a-zA-Z0-9-]+)@(?<currentValue>[^']+)'\\)",
+            "'(?<depName>[a-zA-Z0-9-]+)@(?<currentValue>[^']+)'",
           ],
           depTypeTemplate:
             "{{#if (eq depType 'addDevDep')}}devDependencies{{else if (eq depType 'addPeerDep')}}peerDependencies{{else}}dependencies{{/if}}",
