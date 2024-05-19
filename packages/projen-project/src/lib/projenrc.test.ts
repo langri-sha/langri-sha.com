5e157f69731cce53d3b80ff7913977022733c1a0
Filip Dupanović <filip.dupanovic@gmail.com>
2024-05-19 21:18:16 +0300

    feat(project): Add stub projen configuration file

---
+++ packages/projen-project/src/lib/projenrc.test.ts
@@ -0,0 +1,26 @@
+import { Project } from 'projen'
+import { synthSnapshot } from 'projen/lib/util/synth'
+import { expect, test } from '@langri-sha/jest-test'
+import { ProjenrcFile } from './projenrc'
+
+test('defaults', () => {
+  const project = new Project({
+    name: 'test-project',
+  })
+
+  new ProjenrcFile(project, {})
+
+  expect(synthSnapshot(project)).toMatchSnapshot()
+})
+
+test('custom filename', () => {
+  const project = new Project({
+    name: 'test-project',
+  })
+
+  new ProjenrcFile(project, {
+    filename: '.projerc.ts',
+  })
+
+  expect(synthSnapshot(project)).toMatchSnapshot()
+})
