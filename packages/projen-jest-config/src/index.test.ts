6246d1324d6f1757acb39e3eff176043baa9f5f5
Filip Dupanović <filip.dupanovic@gmail.com>
2024-05-24 03:14:34 +0300

    feat(jest-config): Add package for configuring Jest

---
+++ packages/projen-jest-config/src/index.test.ts
@@ -0,0 +1,49 @@
+import { synthSnapshot } from 'projen/lib/util/synth'
+import { expect, test } from '@langri-sha/jest-test'
+
+import { Project } from 'projen'
+import { JestConfig } from './index'
+
+test('defaults', () => {
+  const project = new Project({
+    name: 'test-project',
+  })
+
+  new JestConfig(project)
+
+  project.synth()
+  expect(synthSnapshot(project)['jest.config.ts']).toMatchSnapshot()
+})
+
+test('with filename', () => {
+  const project = new Project({
+    name: 'test-project',
+  })
+
+  new JestConfig(project, { filename: 'jest.config.mts' })
+
+  project.synth()
+  expect(synthSnapshot(project)['jest.config.mts']).toMatchSnapshot()
+})
+
+test('with extends', () => {
+  const project = new Project({
+    name: 'test-project',
+  })
+
+  new JestConfig(project, { extends: '@langri-sha/jest-config' })
+
+  project.synth()
+  expect(synthSnapshot(project)['jest.config.ts']).toMatchSnapshot()
+})
+
+test('with config', () => {
+  const project = new Project({
+    name: 'test-project',
+  })
+
+  new JestConfig(project, { config: { testEnvironment: 'node' } })
+
+  project.synth()
+  expect(synthSnapshot(project)['jest.config.ts']).toMatchSnapshot()
+})
