6dd996bc066c206485f2d9ba3789dc80ce2516fa
Filip Dupanović <filip.dupanovic@gmail.com>
2024-05-24 13:18:18 +0300

    feat(prettier): Add support for configuring Prettier

---
+++ packages/projen-prettier/src/index.test.ts
@@ -0,0 +1,72 @@
+import { synthSnapshot } from 'projen/lib/util/synth'
+import { describe, expect, test } from '@langri-sha/jest-test'
+
+import { Project } from 'projen'
+import { Prettier } from './index'
+
+test('defaults', () => {
+  const project = new Project({
+    name: 'test-project',
+  })
+
+  new Prettier(project)
+
+  expect(synthSnapshot(project)).toMatchSnapshot()
+})
+
+test('with filename', () => {
+  const project = new Project({
+    name: 'test-project',
+  })
+
+  new Prettier(project, { filename: 'prettier.config.mjs' })
+
+  expect(synthSnapshot(project)).toMatchSnapshot()
+})
+
+test('with ignore patterns', () => {
+  const project = new Project({
+    name: 'test-project',
+  })
+
+  new Prettier(project, {
+    ignorePatterns: ['.*'],
+  })
+
+  expect(synthSnapshot(project)).toMatchSnapshot()
+})
+
+test('with config', () => {
+  const project = new Project({
+    name: 'test-project',
+  })
+
+  new Prettier(project, { config: { endOfLine: 'lf' } })
+
+  expect(synthSnapshot(project)).toMatchSnapshot()
+})
+
+describe('with extends', () => {
+  test('with no config', () => {
+    const project = new Project({
+      name: 'test-project',
+    })
+
+    new Prettier(project, { extends: '@langri-sha/prettier' })
+
+    expect(synthSnapshot(project)['prettier.config.js']).toMatchSnapshot()
+  })
+
+  test('with config', () => {
+    const project = new Project({
+      name: 'test-project',
+    })
+
+    new Prettier(project, {
+      extends: '@langri-sha/prettier',
+      config: { endOfLine: 'lf' },
+    })
+
+    expect(synthSnapshot(project)['prettier.config.js']).toMatchSnapshot()
+  })
+})
--- packages/projen-prettier/src/index.ts
+++ packages/projen-prettier/src/index.ts
@@ -1,1 +1,67 @@
-export {}
+import serialize from 'serialize-javascript'
+import type { Config } from 'prettier'
+import { FileBase, IResolver, IgnoreFile, Project } from 'projen'
+
+/**
+ * Options for configuring Prettier.
+ */
+export interface PrettierOptions {
+  /**
+   * Name of the Prettier config module.
+   *
+   * @default 'prettier.config.js'
+   */
+  filename?: string
+
+  /**
+   * Name of the Prettier configuration package to extend from.
+   */
+  extends?: string
+
+  /**
+   * Ignore patterns.
+   */
+  ignorePatterns?: string[]
+
+  /**
+   * Prettier configuration object.
+   */
+  config?: Config
+}
+
+export class Prettier extends FileBase {
+  #extends?: string
+  ignore: IgnoreFile
+  config: Config
+
+  constructor(project: Project, options: PrettierOptions = {}) {
+    const filename = options.filename ?? 'prettier.config.js'
+
+    super(project, filename, {
+      readonly: true,
+      marker: true,
+    })
+
+    this.#extends = options.extends
+    this.config = options.config ?? {}
+    this.ignore = new IgnoreFile(project, '.prettierignore', {
+      ignorePatterns: options.ignorePatterns,
+    })
+  }
+
+  protected override synthesizeContent(_: IResolver): string | undefined {
+    const config: Config = {
+      ...this.config,
+    }
+
+    const serialized = serialize(config)
+
+    return `${this.#extends ? `import defaults from '${this.#extends}'\n\n` : ''}/** @type {import("prettier").Config} */
+    const config = {
+      ${this.#extends ? `...defaults,` : ''}
+      ${serialized.slice(1)}
+
+    export default config
+    `
+  }
+}
