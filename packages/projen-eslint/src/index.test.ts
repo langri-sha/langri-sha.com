8c7777142cdebf600f828de1e3caa3ad86f4824a
Filip Dupanović <filip.dupanovic@gmail.com>
2024-05-27 12:07:34 +0300

    feat(eslint): Add component for configuring ESLint

---
+++ packages/projen-eslint/src/index.test.ts
@@ -0,0 +1,86 @@
+import { synthSnapshot } from 'projen/lib/util/synth'
+import { describe, expect, test } from '@langri-sha/jest-test'
+
+import { Project } from 'projen'
+import { ESLint } from './index'
+
+test('defaults', () => {
+  const project = new Project({
+    name: 'test-project',
+  })
+
+  new ESLint(project)
+
+  expect(synthSnapshot(project)['eslint.config.js']).toMatchSnapshot()
+})
+
+test('with filename', () => {
+  const project = new Project({
+    name: 'test-project',
+  })
+
+  new ESLint(project, { filename: 'eslint.config.mjs' })
+
+  expect(synthSnapshot(project)['eslint.config.mjs']).toMatchSnapshot()
+})
+
+test('with ignore patterns', () => {
+  const project = new Project({
+    name: 'test-project',
+  })
+
+  new ESLint(project, { ignorePatterns: ['.*'] })
+
+  expect(synthSnapshot(project)['eslint.config.js']).toMatchSnapshot()
+})
+
+test('with config', () => {
+  const project = new Project({
+    name: 'test-project',
+  })
+
+  new ESLint(project, {
+    config: [
+      {
+        rules: {
+          'unicorn/no-null': 'off',
+        },
+      },
+    ],
+  })
+
+  expect(synthSnapshot(project)['eslint.config.js']).toMatchSnapshot()
+})
+
+describe('with extends', () => {
+  test('without config provided', () => {
+    const project = new Project({
+      name: 'test-project',
+    })
+
+    new ESLint(project, {
+      extends: '@langri-sha/eslint-config',
+    })
+
+    expect(synthSnapshot(project)['eslint.config.js']).toMatchSnapshot()
+  })
+
+  test('with config provided', () => {
+    const project = new Project({
+      name: 'test-project',
+    })
+
+    new ESLint(project, {
+      extends: '@langri-sha/eslint-config',
+      config: [
+        {
+          rules: {
+            'unicorn/no-null': 'off',
+          },
+        },
+      ],
+    })
+
+    expect(synthSnapshot(project)['eslint.config.js']).toMatchSnapshot()
+  })
+})
--- packages/projen-eslint/src/index.ts
+++ packages/projen-eslint/src/index.ts
@@ -1,1 +1,69 @@
-export {}
+import serialize from 'serialize-javascript'
+import { FileBase, IResolver, Project } from 'projen'
+
+/**
+ * Options for configuring ESLint.
+ */
+export interface ESLintOptions {
+  /**
+   * Name of the ESLint config module.
+   *
+   * @default 'eslint.config.js'
+   */
+  filename?: string
+
+  /**
+   * Name of the ESLint configuration package to extend from.
+   */
+  extends?: string
+
+  /**
+   * Ignore patterns.
+   */
+  ignorePatterns?: string[]
+
+  /**
+   * Prettier configuration object.
+   */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  config?: any[]
+}
+
+export class ESLint extends FileBase {
+  #extends?: string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  config: any[]
+  ignorePatterns: string[]
+
+  constructor(project: Project, options: ESLintOptions = {}) {
+    const filename = options.filename ?? 'eslint.config.js'
+
+    super(project, filename, {
+      readonly: true,
+      marker: true,
+    })
+
+    this.#extends = options.extends
+    this.config = options.config ?? []
+    this.ignorePatterns = options.ignorePatterns ?? []
+  }
+
+  protected override synthesizeContent(_: IResolver): string | undefined {
+    const config = [
+      ...(this.ignorePatterns.length
+        ? [
+            {
+              ignores: this.ignorePatterns,
+            },
+          ]
+        : []),
+      ...this.config,
+    ]
+
+    const serialized = serialize(config, {
+      unsafe: true,
+    })
+
+    return `${this.#extends ? `import defaults from '${this.#extends}'\n\n` : ''}export default ${this.#extends ? '[...defaults, ' : '['}${serialized.slice(1)}`
+  }
+}
