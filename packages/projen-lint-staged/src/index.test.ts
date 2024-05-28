476860569cbf240867a1fcd0971dcf28ffa916ed
Filip Dupanović <filip.dupanovic@gmail.com>
2024-05-28 12:33:24 +0300

    feat(lint-staged): Add component for configuring `lint-staged`

---
+++ packages/projen-lint-staged/src/index.test.ts
@@ -0,0 +1,60 @@
+import { synthSnapshot } from 'projen/lib/util/synth'
+import { describe, expect, test } from '@langri-sha/jest-test'
+
+import { Project } from 'projen'
+import { LintStaged } from './index'
+
+test('defaults', () => {
+  const project = new Project({
+    name: 'test-project',
+  })
+
+  new LintStaged(project)
+
+  expect(synthSnapshot(project)['lint-staged.config.js']).toMatchSnapshot()
+})
+
+test('with filename', () => {
+  const project = new Project({
+    name: 'test-project',
+  })
+
+  new LintStaged(project, { filename: 'lint-staged.config.mjs' })
+
+  expect(synthSnapshot(project)['lint-staged.config.mjs']).toMatchSnapshot()
+})
+
+test('with config', () => {
+  const project = new Project({
+    name: 'test-project',
+  })
+
+  new LintStaged(project, { config: { '*': 'prettier' } })
+
+  expect(synthSnapshot(project)['lint-staged.config.js']).toMatchSnapshot()
+})
+
+describe('with extends', () => {
+  test('with no config', () => {
+    const project = new Project({
+      name: 'test-project',
+    })
+
+    new LintStaged(project, { extends: '@langri-sha/lint-staged' })
+
+    expect(synthSnapshot(project)['lint-staged.config.js']).toMatchSnapshot()
+  })
+
+  test('with config', () => {
+    const project = new Project({
+      name: 'test-project',
+    })
+
+    new LintStaged(project, {
+      extends: '@langri-sha/lint-staged',
+      config: { '*': 'prettier' },
+    })
+
+    expect(synthSnapshot(project)['lint-staged.config.js']).toMatchSnapshot()
+  })
+})
--- packages/projen-lint-staged/src/index.ts
+++ packages/projen-lint-staged/src/index.ts
@@ -1,1 +1,62 @@
-export {}
+import serialize from 'serialize-javascript'
+import type { Config } from 'prettier'
+import { FileBase, IResolver, Project } from 'projen'
+
+/**
+ * Options for configuring Prettier.
+ */
+export interface LintStagedOptions {
+  /**
+   * Name of the Prettier config module.
+   *
+   * @default 'lint-staged.config.js'
+   */
+  filename?: string
+
+  /**
+   * Name of the `lint-staged` configuration package to extend from.
+   */
+  extends?: string
+
+  /**
+   * `lint-staged` configuration.
+   */
+  config?: Config
+}
+
+export class LintStaged extends FileBase {
+  #extends?: string
+  config: Config
+
+  constructor(project: Project, options: LintStagedOptions = {}) {
+    const filename = options.filename ?? 'lint-staged.config.js'
+
+    super(project, filename, {
+      readonly: true,
+      marker: true,
+    })
+
+    this.#extends = options.extends
+    this.config = options.config ?? {}
+  }
+
+  protected override synthesizeContent(_: IResolver): string | undefined {
+    const config: Config = {
+      ...this.config,
+    }
+
+    const serialized = serialize(config, {
+      unsafe: true,
+    })
+
+    return `${
+      this.#extends ? `import defaults from '${this.#extends}'\n\n` : ''
+    }/** @type {import('lint-staged').Config} */
+    const config = {
+      ${this.#extends ? `...defaults,` : ''}
+      ${serialized.slice(1)}
+
+    export default config
+    `
+  }
+}
