0fb9b5c7c3c85c52da1bcee011dbfd274a974676
Filip Dupanović <filip.dupanovic@gmail.com>
2024-05-28 12:58:51 +0300

    feat(projen): Configure `lint-staged`
    Adds projen component for configuring `lint-staged`.

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,1 +1,8 @@
-export { default } from '@langri-sha/lint-staged'
+import defaults from '@langri-sha/lint-staged'
+
+/** @type {import('lint-staged').Config} */
+const config = {
+  ...defaults,
+}
+
+export default config
