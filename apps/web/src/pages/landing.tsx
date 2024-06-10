8e84058031b6f826e7c8fa330ec8228975a97733
Filip Dupanović <filip.dupanovic@gmail.com>
2024-06-10 14:09:00 +0300

    feat(web): Add initial background
    Some initial edits to add a snazzier background.

--- apps/web/src/pages/landing.tsx
+++ apps/web/src/pages/landing.tsx
@@ -9,9 +9,11 @@ export const Landing = () => (
   <React.Fragment>
     <Global styles={global} />
     <Root>
-      <Header />
-      <Scene />
-      <Drone />
+      <Root>
+        <Header />
+        <Scene />
+        <Drone />
+      </Root>
     </Root>
   </React.Fragment>
 )
@@ -23,4 +25,33 @@ const Root = styled.div`
   flex-flow: column nowrap;
   align-items: center;
   justify-content: center;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: -20%;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    mix-blend-mode: color-dodge;
+    background:
+      radial-gradient(
+          circle farthest-corner at 0 0,
+          #3d1b6d 30%,
+          #e438dc 65%,
+          #404b8c 80%,
+          #79acbb 110%
+        )
+        no-repeat,
+      radial-gradient(closest-side at 60% 50%, #7f4dad 20%, #000 100%) no-repeat,
+      radial-gradient(
+          circle farthest-corner at 0 0,
+          #3d1b6d 30%,
+          #e438dc 65%,
+          #404b8c 80%,
+          #79acbb 110%
+        )
+        no-repeat,
+      radial-gradient(closest-side at 60% 50%, #7f4dad 20%, #000 100%) no-repeat;
+  }
 `
