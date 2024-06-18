222de8178c56c60157db552397e89b05acbbdfc7
Filip Dupanović <filip.dupanovic@gmail.com>
2024-06-18 12:45:50 +0200

    feat(web): Improve event tracking
    Improves processing tracking events, to make them more reliable to page
    navigations.

--- apps/web/src/components/link.tsx
+++ apps/web/src/components/link.tsx
@@ -19,12 +19,22 @@ export const Link: React.FC<LinkProps> = ({
 }) => {
   // Use the 'useCallback' hook for optimized event handler
   const handleClick: React.MouseEventHandler<HTMLAnchorElement> =
-    React.useCallback(() => {
-      gtag('event', eventAction, {
-        event_category: eventCategory,
-        event_label: eventLabel,
-      })
-    }, [eventCategory, eventAction, eventLabel])
+    React.useCallback(
+      (event) => {
+        event.preventDefault()
+
+        gtag('event', eventAction, {
+          event_category: eventCategory,
+          event_label: eventLabel,
+
+          event_callback: () => {
+            window.location.href = props.href
+          },
+          event_timeout: 2000,
+        })
+      },
+      [eventCategory, eventAction, eventLabel],
+    )
 
   return (
     <a {...props} onClick={handleClick}>
