624d00e545c533c2b0b54b6a8d832601bcde0ccb
Filip Dupanović <filip.dupanovic@gmail.com>
2024-06-21 01:36:03 +0200

    feat(terraform): Vary cache for encoding
    Vary cache based on negotiated encoding.

--- terraform/web/traffic.tf
+++ terraform/web/traffic.tf
@@ -56,6 +56,7 @@ resource "google_compute_backend_bucket" "public" {
   custom_response_headers = [
     "Referrer-Policy: no-referrer",
     "Strict-Transport-Security: max-age=31536000; includeSubDomains",
+    "Vary: Accept-Encoding",
     "X-Content-Type-Options: nosniff",
     "X-Frame-Options: deny",
     "X-XSS-Protection: 1; mode=block"
