988aefd2089d241493db03dfda731a7218109e19
Filip Dupanović <filip.dupanovic@gmail.com>
2024-07-13 23:42:25 +0200

    feat(root): Migrate to ESM
    Migrates the root package to ESM.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,5 +2,5 @@ import defaults from '@langri-sha/eslint-config'
 
 export default [
   ...defaults,
-  { ignores: ['.*', 'dist/', 'renovate.d.ts', 'swcrc.d.ts', '!.projenrc.mts'] },
+  { ignores: ['.*', 'dist/', 'renovate.d.ts', 'swcrc.d.ts', '!.projenrc.ts'] },
 ]
