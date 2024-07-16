6b9db7d277bad534100966f05189ff883e07f176
Filip Dupanović <filip.dupanovic@gmail.com>
2024-07-16 21:46:09 +0200

    feat(eslint-config): Add `eslint-plugin-import`

--- packages/eslint-config/src/eslint-config.d.ts
+++ packages/eslint-config/src/eslint-config.d.ts
@@ -1,3 +1,4 @@
 declare module 'eslint-plugin-unicorn'
+declare module 'eslint-plugin-import'
 declare module 'eslint-plugin-react/configs/recommended.js'
 declare module 'eslint-plugin-react/configs/jsx-runtime.js'
