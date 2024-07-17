7d463cf7da94360dd7ff4c0081eeb0e498dac9f2
Filip Dupanović <filip.dupanovic@gmail.com>
2024-07-17 19:01:02 +0200

    feat(readme): Add `ReadmeFile`

--- packages/projen-readme/src/index.ts
+++ packages/projen-readme/src/index.ts
@@ -1,1 +1,22 @@
-export {}
+import { Project, SampleFile } from 'projen'
+
+/**
+ * Initial `README` for new packages.
+ */
+export interface ReadmeFileOptions {
+  filename?: string
+}
+
+/**
+ * Initial `README` for new packages.
+ */
+export class ReadmeFile extends SampleFile {
+  constructor(
+    project: Project,
+    { filename = 'README.md' }: ReadmeFileOptions = {},
+  ) {
+    super(project, filename, {
+      contents: `# ${project.name}\n`,
+    })
+  }
+}
