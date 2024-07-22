0c421f0a9b91a3315e72c21b34bdd1ab10f1f44d
Filip Dupanović <filip.dupanovic@gmail.com>
2024-07-22 23:48:06 +0200

    feat(projen): Add Git ignore patterns
    Adds Git ignore patterns to subprojects.
    
    Closes #873.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,14 @@
 import defaults from '@langri-sha/eslint-config'
 
-export default [...defaults, {"ignores":["**/.*","**/dist/","**/renovate.d.ts","**/swcrc.d.ts","!.projenrc.ts"]}]
+export default [
+  ...defaults,
+  {
+    ignores: [
+      '**/.*',
+      '**/dist/',
+      '**/renovate.d.ts',
+      '**/swcrc.d.ts',
+      '!.projenrc.ts',
+    ],
+  },
+]
