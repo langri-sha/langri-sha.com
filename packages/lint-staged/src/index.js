e0cc3cb395dd78fe119bfd992d0a7855e2fa4141
Filip Dupanović <filip.dupanovic@gmail.com>
2024-07-30 15:44:21 +0200

    feat(lint-staged): Gracefully degrade when ESLint config is missing

--- packages/lint-staged/src/index.js
+++ packages/lint-staged/src/index.js
@@ -3,25 +3,23 @@
  */
 export default {
   '*.{js,cjs,mjs,jsx,ts,mts,tsx}': async (files) => {
-    let ESLint
-
     try {
-      ESLint = (await import('eslint'))['ESLint']
+      const { ESLint } = await import('eslint')
+
+      const eslintCli = new ESLint()
+
+      /** @type {[string, boolean][]} */
+      const ignored = await Promise.all(
+        files.map(async (file) => [file, await eslintCli.isPathIgnored(file)]),
+      )
+      const filtered = ignored
+        .filter(([, isIgnored]) => !isIgnored)
+        .map(([file]) => `"${file}"`)
+
+      return `eslint --fix ${filtered.join(' ')}`
     } catch {
       return ''
     }
-
-    const eslintCli = new ESLint()
-
-    /** @type {[string, boolean][]} */
-    const ignored = await Promise.all(
-      files.map(async (file) => [file, await eslintCli.isPathIgnored(file)]),
-    )
-    const filtered = ignored
-      .filter(([, isIgnored]) => !isIgnored)
-      .map(([file]) => `"${file}"`)
-
-    return `eslint --fix ${filtered.join(' ')}`
   },
   '*': async (files) => {
     let prettier
