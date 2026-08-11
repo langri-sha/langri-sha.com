769b413fd349a19264d5b411ba10adc3971aa0f1
Filip Dupanovic <filip.dupanovic@gmail.com>
2026-08-11 13:24:00 +0200

    Add a network endpoint group for the preview router

--- terraform/modules/previews-router/main.tf
+++ terraform/modules/previews-router/main.tf
@@ -71,3 +71,15 @@ resource "google_cloud_run_v2_service_iam_member" "previews_router" {
   member = "allUsers"
   role   = "roles/run.invoker"
 }
+
+resource "google_compute_region_network_endpoint_group" "previews_router" {
+  name    = "preview-router-network-endpoint-group"
+  project = var.project
+  region  = var.location
+
+  network_endpoint_type = "SERVERLESS"
+
+  cloud_run {
+    service = google_cloud_run_v2_service.previews_router.name
+  }
+}
