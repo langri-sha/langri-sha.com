1a219beb6c2ff0779b16cf8f0115445e33e9e1d8
Filip Dupanovic <filip.dupanovic@gmail.com>
2026-08-11 13:26:31 +0200

    Let the edge project pull published images
    Images are published to the registry in the build project and pulled
    from the edge project by its Cloud Run service agent.

--- terraform/web/artifact-registry-iam.tf
+++ terraform/web/artifact-registry-iam.tf
@@ -1,3 +1,12 @@
+resource "google_artifact_registry_repository_iam_member" "serverless_robot" {
+  location   = google_artifact_registry_repository.repository["docker"].location
+  project    = module.project["build"].project_id
+  repository = google_artifact_registry_repository.repository["docker"].name
+
+  member = "serviceAccount:service-${module.project["edge"].project_number}@serverless-robot-prod.iam.gserviceaccount.com"
+  role   = "roles/artifactregistry.reader"
+}
+
 resource "google_artifact_registry_repository_iam_member" "github" {
   location   = google_artifact_registry_repository.repository["docker"].location
   project    = module.project["build"].project_id
